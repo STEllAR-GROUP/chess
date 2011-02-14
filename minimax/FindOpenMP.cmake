848a9252aba9cb3c7cf0342f4e11244356da6997
Steven Brandt <sbrandt@cct.lsu.edu>
2011-02-14 22:17:16 +0000

    New approach based on advice by Bryce.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@3178 5079c8e4-9419-0410-93bb-9e5ee278c886

--- minimax/FindOpenMP.cmake
+++ minimax/FindOpenMP.cmake
@@ -9,7 +9,6 @@
 #
 # Supported compilers can be found at http://openmp.org/wp/openmp-compilers/
 
-
 # Copyright 2008, 2009 <André Rigland Brodtkorb> Andre.Brodtkorb@ifi.uio.no
 #
 # Redistribution AND use is allowed according to the terms of the New 
@@ -24,76 +23,20 @@
 include(CheckCXXSourceCompiles)
 include(FindPackageHandleStandardArgs)
 
-set(OpenMP_CXX_FLAG_CANDIDATES
-  #Gnu
-  "-fopenmp"
-  #Intel
-  "-openmp" 
-  #Empty, if compiler automatically accepts openmp
-  " "
-)
+message(STATUS "Compiler is ${CMAKE_C_COMPILER_ID}")
 
-set(OpenMP_LINK_FLAG_CANDIDATES
-  #Gnu
-  "-lgomp"
-  #Intel
-  "-liomp5 -lpthread"
-  #Empty, if linker is really smart
-  " "
-)
-
-# sample openmp source code to test
-set(OpenMP_CXX_TEST_SOURCE 
-"
-#include <omp.h>
-int main() { 
-omp_set_num_threads(1);
-return omp_get_num_threads();
-}
-")
-# if these are set then do not try to find them again,
-# by avoiding any try_compiles for the flags
-if(DEFINED OpenMP_CXX_FLAGS)
-  set(OpenMP_CXX_FLAG_CANDIDATES)
-endif(DEFINED OpenMP_CXX_FLAGS)
-
-if (DEFINED OpenMP_LINK_FLAGS)
-  set(OpenMP_LINK_FLAG_CANDIDATES)
-endif(DEFINED OpenMP_LINK_FLAGS)
-
-# check cxx compiler
-foreach(FLAG ${OpenMP_CXX_FLAG_CANDIDATES})
-  set(SAFE_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
-  set(CMAKE_REQUIRED_FLAGS "${FLAG}")
-  set(OpenMP_FLAG_DETECTED)
-  message(STATUS "Try OpenMP CXX flag = [${FLAG}]")
-
-  foreach(LINK ${OpenMP_LINK_FLAG_CANDIDATES})
-    set(CMAKE_REQUIRED_LIBRARIES "${LINK}")
-    check_cxx_source_compiles("${OpenMP_CXX_TEST_SOURCE}" OpenMP_FLAG_DETECTED)
-    set(CMAKE_REQUIRED_FLAGS "${SAFE_CMAKE_REQUIRED_FLAGS}")
-    if(OpenMP_FLAG_DETECTED)
-      set(OpenMP_CXX_FLAGS_INTERNAL "${FLAG}")
-      set(OpenMP_LINK_FLAGS_INTERNAL "${LINK}")
-      message(STATUS "Found a working combination")
-      set(BREAK "please")
-      break()
-    endif(OpenMP_FLAG_DETECTED)
-  endforeach(LINK ${OpenMP_LINK_FLAG_CANDIDATES})
-  if(BREAK)
-    break()
-  endif(BREAK)
-endforeach(FLAG ${OpenMP_CXX_FLAG_CANDIDATES})
-
-set(OpenMP_CXX_FLAGS "${OpenMP_CXX_FLAGS_INTERNAL}"
-  CACHE STRING "C++ compiler flags for OpenMP parallization")
-set(OpenMP_LINK_FLAGS "${OpenMP_LINK_FLAGS_INTERNAL}"
-  CACHE STRING "C++ linker flags for OpenMP parallization")
-# handle the standard arguments for find_package
-find_package_handle_standard_args(OpenMP DEFAULT_MSG 
-  OpenMP_CXX_FLAGS )
-
-mark_as_advanced(
-  OpenMP_CXX_FLAGS
-  OpenMP_LINK_FLAGS
-)
+set(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS TRUE)
+if(${CMAKE_C_COMPILER_ID} STREQUAL "Intel")
+    set(OpenMP_C_FLAGS "-openmp")
+    set(OpenMP_LINK_FLAGS "-liomp5 -lpthread")
+else()
+    set(OpenMP_C_FLAGS "-fopenmp")
+    set(OpenMP_LINK_FLAGS "-lgomp")
+endif() 
+if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
+    set(OpenMP_CXX_FLAGS "-openmp")
+    set(OpenMP_LINK_FLAGS "-liomp5 -lpthread")
+else()
+    set(OpenMP_CXX_FLAGS "-fopenmp")
+    set(OpenMP_LINK_FLAGS "-lgomp")
+endif() 
