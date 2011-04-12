28ce1b99e5626d85a805d803d7c548044cffe0a6
Steven Brandt <sbrandt@cct.lsu.edu>
2011-04-12 16:32:28 +0000

    Add score checking
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@3790 5079c8e4-9419-0410-93bb-9e5ee278c886

--- openmp/harness.pl
+++ openmp/harness.pl
@@ -2,24 +2,70 @@ use strict;
 use FileHandle;
 
 my $anskey = {};
+my $scoretab = {};
+
+#Answer key:
+
+#board1
+#ply | answer
+#====+=======
+#  7 |   e2e4
+#  6 |   b1c3
+#  5 |   b2b4
+#  4 |   g1f3
+#  3 |   g1f3
+#  2 |   d2d4
 
 $anskey->{1}->{2} = "e2e4";
 $anskey->{1}->{3} = "e2e4";
 $anskey->{1}->{4} = "g1f3";
+$anskey->{1}->{5} = "b2b4";
+
+#board2
+#ply | answer
+#====+=======
+#  7 |   d1b3
+#  6 |   c1d2
+#  5 |   c1g5
+#  4 |   c1g5
+#  3 |   c1d2
+#  2 |   e2e4
 
 $anskey->{2}->{2} = "e2e4";
 $anskey->{2}->{3} = "c1d2";
 $anskey->{2}->{4} = "c1g5";
+$anskey->{2}->{5} = "c1g5";
+
+#board3
+#ply | answer
+#====+=======
+#  7 |   f5h7
+#  6 |   f5g7
+#  5 |   f5h6
+#  4 |   f5g7
+#  3 |   f3b3
+#  2 |   b2b3
 
 $anskey->{3}->{2} = "b2b3";
 $anskey->{3}->{3} = "f3b3";
 $anskey->{3}->{4} = "f5g7";
+$anskey->{3}->{5} = "f5h6";
+
+#board4
+#ply | answer
+#====+=======
+#  7 |   e5f5
+#  6 |   e5e3
+#  5 |   h5h6
+#  4 |   e5e3
+#  3 |   e5b5
+#  2 |   e5e3
 
 $anskey->{4}->{2} = "e5e1";
 $anskey->{4}->{3} = "g5f7";
 $anskey->{4}->{4} = "e5e1";
+$anskey->{4}->{5} = "h5h6";
 
-printf("%10s,%5s, %4s, %5s, %s, %s\n","method","board","ply","score","ansr","status");
 for my $sm (("minimax","alphabeta","mtd-f")) {
     for(my $b=1;$b<=4;$b++) {
         for(my $ply=2;$ply<=4;$ply++) {
@@ -40,8 +86,17 @@ for my $sm (("minimax","alphabeta","mtd-f")) {
             if(defined($anskey->{$b}->{$ply})) {
                 my $v = $anskey->{$b}->{$ply};
                 if($ans ne $v) {
-                    $st = "ERROR($v)";
+                    $st = "MOVE($v)";
                 }
+            }
+            if(defined($scoretab->{$b}->{$ply})) {
+                my $s = $scoretab->{$b}->{$ply};
+                if($s != $score) {
+                    $st .= " SCORE($s)";
+                }
+            }
+            if($st eq "OK") {
+                $scoretab->{$b}->{$ply} = $score;
             }
             printf("%10s, %4d, %4d, %5d, %s, %s\n",$sm,$b,$ply,$score,$ans,$st);
             close($fd);
