83bdbe2fb8a676e08651e40b5cefe914b1611595
Steven Brandt <sbrandt@cct.lsu.edu>
2011-04-13 03:11:20 +0000

    This expands the verification table. There seems to be a problem with board4 ply 5. Minimax and alpha-beta give different moves (but the same score).
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@3801 5079c8e4-9419-0410-93bb-9e5ee278c886

--- openmp/harness.pl
+++ openmp/harness.pl
@@ -19,7 +19,8 @@ my $scoretab = {};
 $anskey->{1}->{2} = "e2e4";
 $anskey->{1}->{3} = "e2e4";
 $anskey->{1}->{4} = "g1f3";
-$anskey->{1}->{5} = "b2b4";
+$anskey->{1}->{5} = "e2e4";
+$anskey->{1}->{6} = "b1c3";
 
 #board2
 #ply | answer
@@ -35,6 +36,7 @@ $anskey->{2}->{2} = "e2e4";
 $anskey->{2}->{3} = "c1d2";
 $anskey->{2}->{4} = "c1g5";
 $anskey->{2}->{5} = "c1g5";
+$anskey->{2}->{6} = "c1d2";
 
 #board3
 #ply | answer
@@ -50,6 +52,7 @@ $anskey->{3}->{2} = "b2b3";
 $anskey->{3}->{3} = "f3b3";
 $anskey->{3}->{4} = "f5g7";
 $anskey->{3}->{5} = "f5h6";
+$anskey->{3}->{6} = "f5g7";
 
 #board4
 #ply | answer
@@ -64,38 +67,57 @@ $anskey->{3}->{5} = "f5h6";
 $anskey->{4}->{2} = "e5e1";
 $anskey->{4}->{3} = "g5f7";
 $anskey->{4}->{4} = "e5e1";
-$anskey->{4}->{5} = "h5h6";
+$anskey->{4}->{5} = "e5f6";
+$anskey->{4}->{6} = "e5e3";
+
+my $bad_score = -6666;
 
 for my $sm (("minimax","alphabeta","mtd-f")) {
     for(my $b=1;$b<=4;$b++) {
-        for(my $ply=2;$ply<=4;$ply++) {
+        for(my $ply=2;$ply<=5;$ply++) {
+            # It takes too long for minimax above ply 4
+            # So I ran it once at 5 to verify the answer
+            # and then introduced this next.
+            if($sm eq "minimax" and $ply >= 5) {
+                next;
+            }
             genbench($sm,$b,$ply);
             my $fd = new FileHandle;
             open($fd,"./src/chx -s .bench.ini 2>/dev/null|");
+
             my $ans = "";
-            my $score = -666;
+            my $score = $bad_score;
+
+            my $st = "OK ";
             while(<$fd>) {
                 if(/Computer's move: ([a-h][1-8][a-h][1-8])/) {
+                    if($ans ne "" and $ans ne $1) {
+                        $st = "VARIABLE($ans != $1) ";
+                        last;
+                    }
                     $ans = $1;
                 }
                 if(/SCORE=(-?\d+)/) {
+                    if($score != $bad_score and $score != $1) {
+                        $st = "VARIABLE($score != $1) ";
+                        last;
+                    }
                     $score=$1;
                 }
             }
-            my $st = "OK";
             if(defined($anskey->{$b}->{$ply})) {
                 my $v = $anskey->{$b}->{$ply};
                 if($ans ne $v) {
-                    $st = "MOVE($v)";
+                    $st .= "MOVE($v) ";
                 }
             }
             if(defined($scoretab->{$b}->{$ply})) {
                 my $s = $scoretab->{$b}->{$ply};
                 if($s != $score) {
-                    $st .= " SCORE($s)";
+                    $st .= "SCORE($s) ";
                 }
             }
-            if($st eq "OK") {
+            if($st eq "OK ") {
                 $scoretab->{$b}->{$ply} = $score;
             }
             printf("%10s, %4d, %4d, %5d, %s, %s\n",$sm,$b,$ply,$score,$ans,$st);
@@ -112,6 +134,9 @@ sub genbench {
     my $b = shift;
     my $ply = shift;
     my $fd = new FileHandle;
+    my $runs = 3;
+    $runs = 1 if($sm eq "minimax");
+    $runs = 1 if($ply > 5);
     open($fd,">.bench.ini");
 print $fd qq{
 ; settings.ini
@@ -167,7 +192,7 @@ file=inputs/board$b
 
 max_ply=$ply
 
-num_runs=1
+num_runs=$runs
 };
     close($fd);
 }
