fa416f05f67fd2a13c1b74a662cdd9777ae21182
Steven Brandt <sbrandt@cct.lsu.edu>
2011-06-22 03:13:49 +0000

    Avoid false errors from valgrind
    allow use of operator[] in const form
    
    
    
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@4659 5079c8e4-9419-0410-93bb-9e5ee278c886

--- openmp/headers/FixedVec.hpp
+++ openmp/headers/FixedVec.hpp
@@ -6,26 +6,35 @@ struct FixedVec {
     T data[N];
     int _size;
     FixedVec() : _size(0) {}
-    FixedVec(int _sz) : _size(_size) {
-        assert(_size <= N);
-    }
     T& operator[](int n) {
         assert(n >= 0 && n < _size);
         return data[n];
     }
-    const T& get(int n) const {
+    const T& operator[](int n) const {
         assert(n >= 0 && n < _size);
         return data[n];
     }
     T *ptr() { return data; }
     void push_back(T t) {
-        assert(_size < N);
-        data[_size++] = t;
+        if(_size == N) {
+            // We're only using this for
+            // move history, and only the
+            // last N moves matter.
+            for(int i=1;i<N;i++)
+                data[i-1] = data[i];
+            data[_size-1] = t;
+        } else {
+            data[_size++] = t;
+        }
     }
     int size() const {
         return _size;
     }
     int resize(int n) {
+        // Keep Valgrind happy
+        while(_size < n) {
+            push_back(0);
+        }
         _size = n;
         assert(n >= 0 && n < N);
     }
