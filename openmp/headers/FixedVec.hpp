045a1b1f3d2e848c317e8e6278c750389fdf209a
Steven Brandt <sbrandt@cct.lsu.edu>
2011-06-25 00:53:02 +0000

    Add copyright notices.
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@4673 5079c8e4-9419-0410-93bb-9e5ee278c886

--- openmp/headers/FixedVec.hpp
+++ openmp/headers/FixedVec.hpp
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Steve Brandt and Philip LeBlanc
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
 #ifndef FIXED_VEC_HPP
 #define FIXED_VEC_HPP 1
 #include <assert.h>
--- openmp/src/board.cpp
+++ openmp/src/board.cpp
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Steve Brandt and Philip LeBlanc
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
 /*
  *  board.cpp
  */
@@ -5,6 +11,7 @@
 
 #include "board.hpp"
 #include "here.hpp"
+#include <string.h>
 
 static int count, count2;
 // Make sure hashing works. Note
