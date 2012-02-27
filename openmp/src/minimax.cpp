224076aadace6cf3c590fd8821e43ee341c6d80b
Phillip LeBlanc <pleblanc@cct.lsu.edu>
2012-02-27 21:58:16 +0000

    Adding Boost header to minimax.cpp and alphabeta.cpp
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@7190 5079c8e4-9419-0410-93bb-9e5ee278c886

--- openmp/src/minimax.cpp
+++ openmp/src/minimax.cpp
@@ -1,3 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2012 Steve Brandt and Phillip LeBlanc
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
 #include "parallel_support.hpp"
 #include "search.hpp"
 #include <assert.h>
