3876b06a11fc36bd7ddc4a8587ce710053531eb7
Phillip LeBlanc <pleblanc@cct.lsu.edu>
2012-04-05 21:26:55 +0000

    Taking a stab at serialization
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@7662 5079c8e4-9419-0410-93bb-9e5ee278c886

--- openmp/headers/chx_hpx.hpp
+++ openmp/headers/chx_hpx.hpp
@@ -5,48 +5,34 @@
 #include <hpx/hpx_init.hpp>
 #include <hpx/runtime/actions/plain_action.hpp>
 #include "node.hpp"
-
-//typedef hpx::actions::plain_result_action1<
-//    score_t,           // return type
-//    search_info*,      // arguments
-//    search             // function name
-//> minimax_action;
-    
-//typedef hpx::actions::plain_result_action1<
-//    score_t,           // return type
-//    search_info*,      // arguments
-//    qeval             // function name
-//> qeval_action;
-    
-    
-//HPX_REGISTER_PLAIN_ACTION(minimax_action);    
-//HPX_REGISTER_PLAIN_ACTION(qeval_action);    
+#include "defs.hpp"
     
 namespace boost { namespace serialization
 {
 template <typename Archive>
 void serialize(Archive &ar, search_info& info, const unsigned int)
 {
-    
+    ar & info.board;
+    ar & info.result;
+    ar & info.depth;
+    ar & info.incr;
+    ar & info.alpha;
+    ar & info.beta;
 }
 template <typename Archive>
-void serialize(Archive &ar, smart_ptr<search_info>& info, const unsigned int)
-{}
-template <typename Archive>
-void serialize(Archive &ar, smart_ptr<task>& info, const unsigned int)
-{}
-template <typename Archive>
 void serialize(Archive &ar, node_t& board, const unsigned int)
-{}
-template <typename Archive>
-void serialize(Archive &ar, chess_move& m, const unsigned int)
-{}
-template <typename Archive>
-void serialize(Archive &ar, pthread_mutex_t mut, const unsigned int)
-{}
-template <typename Archive>
-void serialize(Archive &ar, pthread_cond_t c, const unsigned int)
-{}
+{
+    ar & board.hash;
+    ar & board.color;
+    ar & board.piece;
+    ar & board.depth;
+    ar & board.side;
+    ar & board.castle;
+    ar & board.ep;
+    ar & board.fifty;
+    ar & board.ply;
+    ar & board.hply;
+}
 }}
 
 #endif
