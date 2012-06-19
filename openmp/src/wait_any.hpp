01eb2179d23a2e6c2ca7d683206a2929b49343a7
Alexander Duchene <aduchene@cct.lsu.edu>
2012-06-19 01:27:52 +0000

    Improved wait_any but will be updated for new coordination struct
    git-svn-id: https://svn.cct.lsu.edu/repos/projects/parallex/branches/chess@8249 5079c8e4-9419-0410-93bb-9e5ee278c886

--- openmp/src/wait_any.hpp
+++ openmp/src/wait_any.hpp
@@ -30,7 +30,7 @@ smart_ptr<task> wait_any_busy(std::vector< smart_ptr<task> >& task_list){
 }
 
 
-int get_done_task(std::vector< smart_ptr<task> > const& task_list){
+int get_done_index(std::vector< smart_ptr<task> > const& task_list){
    for(int i=0;i<task_list.size();i++){
       if(task_list[i]->info->par_done){
          return i;
@@ -39,57 +39,23 @@ int get_done_task(std::vector< smart_ptr<task> > const& task_list){
    return -1;
 }
 
-smart_ptr<task> wait_any_timeout(std::vector< smart_ptr<task> >& task_list){
-   int done_index=get_done_task(task_list);
-   if(done_index<0){ //no task already done
-      //create shared mutex and condition variable for tasks to signal completion with
-      pthread_mutex_t shared_mut;
-      pthread_cond_t shared_cond;
-      pthread_mutex_init(&shared_mut,0);
-      pthread_cond_init(&shared_cond,0);
-      //set task's to share cond/mut 
-      for(int i=0;i<task_list.size();i++){
-         task_list[i]->info->mut=shared_mut;
-         task_list[i]->info->cond=shared_cond;
-      }
-      //timed cond wait because task may have already completed while mutex/cond was being initialized, so don't want to wait forever
-      pthread_mutex_lock(&shared_mut);
-      struct timeval now;
-      struct timespec timeout;
-      gettimeofday(&now,0);
-      timeout.tv_sec=now.tv_sec+1;
-      timeout.tv_nsec=now.tv_usec*1000;
-      int wait_return_code=pthread_cond_timedwait(&shared_cond,&shared_mut,&timeout);
-      if(wait_return_code==ETIMEDOUT){
-         return wait_any_timeout(task_list);
-      }
-      done_index=get_done_task(task_list);
+smart_ptr<task> wait_any(std::vector< smart_ptr<task> >& task_list){
+   pthread_mutex_t* shared_mut=task_list[0]->info->mut;
+   pthread_cond_t* shared_cond=task_list[0]->info->cond;
+   int lock_return=pthread_mutex_lock(shared_mut);
+   int done_index=get_done_index(task_list);
+   if(done_index<0){
+      //std::cout<<"cond-wait'ing"<<std::endl;
+      pthread_cond_wait(shared_cond,shared_mut);
+      //std::cout<<"Done waiting"<<std::endl;
+      done_index=get_done_index(task_list);
    }
    smart_ptr<task> to_ret=task_list[done_index];
    task_list.erase(task_list.begin()+done_index);
+   pthread_mutex_unlock(shared_mut);
    return to_ret;
 }
 
-smart_ptr<task> wait_any(std::vector< smart_ptr<task> >& task_list){
-   pthread_mutex_t shared_mut;
-   pthread_cond_t shared_cond;
-   pthread_mutex_init(&shared_mut,0);
-   pthread_cond_init(&shared_cond,0);
-   for(int i=0;i<task_list.size();i++){
-      task_list[i]->info->mut=shared_mut;
-      task_list[i]->info->cond=shared_cond;
-   }
-   pthread_mutex_lock(&shared_mut);
-   int done_index=get_done_task(task_list);
-   if(done_index<0){
-      pthread_cond_wait(&shared_cond,&shared_mut);
-      done_index=get_done_task(task_list);
-   }
-   smart_ptr<task> to_ret=task_list[done_index];
-   task_list.erase(task_list.begin()+done_index);
-   pthread_mutex_unlock(&shared_mut);
-   return to_ret;
-}
 
 
 #endif
